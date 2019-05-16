e7ae10e7d743ab20b927267a49c34d09ae871135
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-05-16 02:03:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,4 @@
 def oxford_comma(array)
   array.length == 1 ? array.join : array.length == 2 ? array.join(" and ") :
-  array[0..array.length-3].join(", ") + array[-2..-1].join(" and ")  
+  array[0..array.length-3].join(", ") + array[-2..-1].join(" and ")
 end
